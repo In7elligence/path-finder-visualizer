2fd36cbc980b00ab06a70d3520f55c6347e2509e
Martin Beck Andersen <martin_bandersen@hotmail.com>
2025-02-17 22:14:32 +0000

    Fixed sitemap issue
    Fixed issue with sitemap so that it now displays properly.

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -3,30 +3,25 @@ import { GetServerSideProps } from "next";
 const generateSitemap = async (): Promise<string> => {
   return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <!-- Homepage -->
-  <url>
+    <url>
       <loc>https://martin-pathfinding-visualizer.vercel.app/</loc>
-      <lastmod>2025-02-17</lastmod>
+      <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
       <changefreq>yearly</changefreq>
       <priority>1.0</priority>
-  </url>
+    </url>
   </urlset>`;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export const getServerSideProps: GetServerSideProps<{}> = async (ctx) => {
-  ctx.res.setHeader("Content-Type", "xml");
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  ctx.res.setHeader("Content-Type", "text/xml");
   const xml = await generateSitemap();
   ctx.res.write(xml);
   ctx.res.end();
 
-  return {
-    props: {},
-  };
+  return { props: {} };
 };
 
-const sitemap = () => {
+// Default export required (no JSX needed)
+export default function Sitemap() {
   return null;
-};
-
-export default sitemap;
+}
