ec67ccaa10540ef33b08f265f438383574339e85
Martin Beck Andersen <martin_bandersen@hotmail.com>
2025-02-21 14:26:19 +0000

    Changed the import of Next.js Analytics component
    Changed the import from: @vercel/analytics/react to: @vercel/analytics/next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = localFont({
   src: [
